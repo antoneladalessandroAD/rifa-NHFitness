62f44b9aa2ff3099ae17f7099f8cb85c4247c07b
antoneladalessandroAD <antonela.dalessandro@icloud.com>
2026-07-03 00:45:51 +0000

    corrige build Versel

--- src/components/Rifa.jsx
+++ src/components/Rifa.jsx
@@ -9,7 +9,7 @@ import {
 import { db } from "../firebase/config";
 import Numero from "./Numero";
 import Formulario from "./Formulario";
-import logo from "./logo.png";
+import logo from "./logo.jpg";
 
 function Rifa() {
   const [numeros, setNumeros] = useState([]);
